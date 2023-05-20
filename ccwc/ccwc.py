db9fd26e572c533a36cf32f7c20d9394364b5038
Joe Paris <2662123+joeparis@users.noreply.github.com>
2023-05-20 14:04:39 -0700

    Implement word counting

--- ccwc/ccwc.py
+++ ccwc/ccwc.py
@@ -10,8 +10,9 @@ import click
 @click.argument("file")
 @click.option("-c", "--bytes", is_flag=True, help="Print the byte counts.")
 @click.option("-l", "--lines", is_flag=True, help="Print the byte counts.")
+@click.option("-w", "--words", is_flag=True, help="Print the byte counts.")
 @click.version_option(version="0.1.0")
-def cli(file, bytes, lines):
+def cli(file, bytes, lines, words):
     file_path = Path(file)
 
     if bytes:
@@ -21,6 +22,15 @@ def cli(file, bytes, lines):
     if lines:
         with file_path.open(encoding="utf-8") as f:
             click.echo(f"{len(f.readlines())} {file_path.name}")
+
+    if words:
+        count = 0
+        with file_path.open(encoding="utf-8") as f:
+            _lines = f.readlines()
+            for line in _lines:
+                count += len(line.strip().split())
+
+        click.echo(f"{count} {file_path.name}")
 
 
 def main():
